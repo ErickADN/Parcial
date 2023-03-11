15e0465da72ba098bd9fffd2b4fef77cb07ba0d9
Jessica <Jessica@DESKTOP-GVB16UV>
2023-03-11 11:14:49 -0500

    Proyecto del examen parcial terminado

---
+++ src/main/java/api/cinestar/repository/ICine.java
@@ -0,0 +1,14 @@
+package api.cinestar.repository;
+
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import api.cinestar.model.Cine;
+
+@Repository
+public interface ICine extends JpaRepository<Cine, Integer>{
+	@Query(value ="{call sp_getCine(:id)}", nativeQuery=true)
+	Cine getCine(int id);
+}
---
+++ src/test/java/api/cinestar/ApiCinesStarApplicationTests.java
@@ -0,0 +1,13 @@
+package api.cinestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCinesStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
